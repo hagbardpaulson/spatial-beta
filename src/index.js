ccbdaa864971b1503285f3a16f08355995fa673a
Hagbard Paulson <hagbardpaulson@hotmail.com>
2019-10-17 14:29:24 -0800

    0.1.1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-export * from './components'
-export * from './install'
+// export * from './components'
+// export * from './install'
+
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
+// Export components individually
+export { ComponentA, ComponentB };
+
+// What should happen if the user installs the library as a plugin
+function install(Vue) {
+    Vue.component("component-a", ComponentA);
+    Vue.component("component-b", ComponentB);
+}
+
+// Export the library as a plugin
+export default { install };
