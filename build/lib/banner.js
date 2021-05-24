781d6ddaa2bd92f770df30f498a231728b384161
Hagbard Paulson <hagbardpaulson@hotmail.com>
2021-05-23 16:10:41 -0800

    build: 0.1.9062

--- build/lib/banner.js
+++ build/lib/banner.js
@@ -2,7 +2,7 @@ import { pack } from "../config";
 
 const version = process.env.VERSION || pack.version;
 const banner = `/*!
- * ${pack.name} v${version}
+ * spatial design v${version}
  * Made with <3 ${new Date().getFullYear()}
  * Released under the MIT License.
  */`;
